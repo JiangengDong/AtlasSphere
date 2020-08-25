af18a1a58fe11f1b6a7f6e3c126f72a204846a2a
Jiangeng Dong <dongjiangeng@gmail.com>
2020-08-25 04:15:19 +0000

    use percomputed testing data

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,17 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <boost/filesystem.hpp>
+
+Eigen::MatrixXd read_dataset(const std::string& filename) {
+    std::ifstream input_file(filename);
+    Eigen::MatrixXd m(1000, 3);
+    for (size_t i = 0; i < 1000; i++) {
+        input_file >> m(i, 0) >> m(i, 1) >> m(i, 2);
+    }
+    input_file.close();
+    return m;
+}
 
 int main(int argc, char **argv) {
     ompl::msg::setLogLevel(ompl::msg::LOG_NONE);
@@ -15,26 +26,20 @@ int main(int argc, char **argv) {
     srand(0);
     SpherePlanning instance(param);
     Eigen::Vector3d start, goal;
+    Eigen::MatrixXd starts = read_dataset("./data/training_samples/starts.txt");
+    Eigen::MatrixXd goals = read_dataset("./data/training_samples/goals.txt");
 
-    std::ofstream len_file("/workspaces/AtlasSphere/data/result/rrtstar-mpnet-time.csv");
+    std::ofstream len_file("./data/result/test.csv");
     for (size_t iter=0; iter<50; iter++){
         // sample start and goal
-        do{
-            start = Eigen::Vector3d::Random();
-            start /= start.norm();
-        }while(!instance.isValid(start));
-        do {
-            goal = Eigen::Vector3d::Random();
-            goal /= goal.norm();
-        }while(!instance.isValid(goal));
-        instance.setStartGoal(start, goal);
+        instance.setStartGoal(starts.row(iter), goals.row(iter));
 
         // repeat 2 sec planning for 10 times
         ompl::base::PlannerStatus status;
         double total_time=0;
         double length = std::numeric_limits<double>::infinity();
         std::cout << "experiment " << iter << std::endl;
-        for (size_t i = 0; i< 5; i++){
+        for (size_t i = 0; i< 10; i++){
             status = instance._simple_setup->solve(2);
             total_time += instance._simple_setup->getLastPlanComputationTime();
             if (status == ompl::base::PlannerStatus::EXACT_SOLUTION) {

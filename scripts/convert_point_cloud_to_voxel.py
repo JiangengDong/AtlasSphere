e410044a01e2e3e52c6eda01b4f485a3b3f5dcd6
Jiangeng Dong <dongjiangeng@gmail.com>
2021-03-30 00:29:21 -0700

    convert point cloud to voxel

--- scripts/convert_point_cloud_to_voxel.py
+++ scripts/convert_point_cloud_to_voxel.py
@@ -1,5 +1,20 @@
 import open3d as o3d
+import numpy as np
 
 
 def convert_point_cloud_to_voxel(point_cloud_path: str, voxel_path: str):
-    pass
+    points = np.load(point_cloud_path)
+    pt = o3d.geometry.PointCloud()
+    pt.points = o3d.utility.Vector3dVector(points)
+    voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud_within_bounds(pt, 0.05, np.array([-1.0, -1.0, -1.0]), np.array([1.0, 1.0, 1.0]))
+    voxel_indices = np.stack([voxel.grid_index for voxel in voxel_grid.get_voxels()])
+    converted_voxel = np.zeros((40, 40, 40), dtype=np.float32)
+    converted_voxel[voxel_indices[:, 0], voxel_indices[:, 1], voxel_indices[:, 2]] = 1.0
+    np.save(voxel_path, converted_voxel)
+
+def main():
+    for i in range(20):
+        convert_point_cloud_to_voxel("./data/point_cloud/env{}.npy".format(i), "./data/voxel/env{}.npy".format(i))
+
+if __name__ == "__main__":
+    main()
